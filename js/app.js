2e0bbaf37dd4c5bcd33f564d7ed8a1e9a463782a
Brandon <brandon@structur.co>
2019-05-12 00:25:29 -0400

    add git ignore

---
+++ js/app.js
@@ -0,0 +1,40 @@
+var intro = document.getElementById('intro');
+var windowHeight, windowWidth
+var me = document.getElementById('me');
+
+function SmoothScrollTo(e, time, where) {
+    var eAmt = e.getBoundingClientRect().top / 100;
+    var curTime = 0;
+    while (curTime <= time) {
+        window.setTimeout(scroll, curTime, eAmt, where);
+        curTime += time / 100;
+    }
+}
+
+function scroll(eAmt, where = "center") {
+    if(where == "center" || where == "")
+        window.scrollBy(0, eAmt / 2);
+    if (where == "top")
+        window.scrollBy(0, eAmt);
+}
+
+function scrollToSalutaion() {
+	SmoothScrollTo(me, 325, "top");
+}
+
+function getCurrentWindowDimensions() {
+	windowHeight = window.innerHeight;
+	windowWidth = window.innerWidth;
+	console.log(windowWidth + ' x ' + windowHeight);
+}
+
+function resize() {
+	getCurrentWindowDimensions();
+	intro.style.height = windowHeight + 'px';
+}
+
+//listen for resize event.
+window.onresize = resize;
+
+// initalize intro dimensions
+resize();
